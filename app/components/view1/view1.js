dbe9c796b43015c84c1b308e0e5800876dc4ad66
lchayoun <liel.chayoun@intel.com>
2016-11-12 15:49:54 +0200

    added basic labels upload

--- app/components/view1/view1.js
+++ app/components/view1/view1.js
@@ -2,52 +2,181 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'components/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs: 'vm'
-  });
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+        templateUrl: 'components/view1/view1.html',
+        controller: 'View1Ctrl',
+        controllerAs: 'vm'
+    });
 }])
 
-.controller('View1Ctrl', ['$scope', '$document', function($scope, $document) {
-	var vm = this;
-	vm.text = "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" + 
-				"bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla";
+.controller('View1Ctrl', [function () {
+    var vm = this;
 
+    vm.state = 0; //before any load
 
-	vm.updateSelectedText = function() {
-		var flag = 0;
-		var sel = window.getSelection();
-		var startOffset = -1;
-		var endOffset = -1;
+    vm.loadLabelsDB = loadLabelsDB;
+    vm.loadText = loadText;
+    vm.updateSelectedText = updateSelectedText;
+    vm.updateFilter = updateFilter;
+    vm.tag = tag;
 
-		for (var i = 0; i < sel.rangeCount; i++) {
-		    var s = sel.getRangeAt(i).startContainer.parentNode.id;
-		    var e = sel.getRangeAt(i).endContainer.parentNode.id;
-		    // console.log(i,s,e,sel);
-		    if (s == "oren") flag = 1;
-		    if (flag = 1 && e == "oren" || e == "child") flag = 2;
-		    if (flag == 2) {
-				startOffset = sel.getRangeAt(i).startOffset;
-				endOffset = sel.getRangeAt(i).endOffset;
-		    }
-		}
+    activate();
 
-		if (flag == 2) {
-			console.log(vm.text.substring(startOffset, endOffset))
-		} 
-	}
+    //////////////////////////////////////////
 
+    function activate() {
+
+    }
+
+    function loadLabelsDB() {
+        vm.state = 1;
+        vm.labelsDBjson = JSON.parse(vm.labelsDB);
+        vm.labelsDBjsonFiltered = {"collection": []};
+    }
+
+    function loadText() {
+        vm.state = 2;
+    }
+
+    function updateSelectedText() {
+        var flag = 0;
+        var sel = window.getSelection();
+        vm.startOffset = -1;
+        vm.endOffset = -1;
+
+        for (var i = 0; i < sel.rangeCount; i++) {
+            var s = sel.getRangeAt(i).startContainer.parentNode.id;
+            var e = sel.getRangeAt(i).endContainer.parentNode.id;
+            // console.log(i,s,e,sel);
+            if (s == "oren") flag = 1;
+            if (flag = 1 && e == "oren" || e == "child") flag = 2;
+            if (flag == 2) {
+                vm.startOffset = sel.getRangeAt(i).startOffset;
+                vm.endOffset = sel.getRangeAt(i).endOffset;
+            }
+        }
+        if (flag == 2) {
+            //console.log(vm.text.substring(startOffset, endOffset))
+        }
+    }
+
+    function updateFilter() {
+        vm.labelsDBjsonFiltered = {"collection": []};
+        vm.labelsDBjsonFiltered.collection = [];
+        if (vm.filter.length > 0) {
+            vm.labelsDBjson.collection.forEach(function (labelObj) {
+                labelObj.labels.every(function (label) {
+                    if (label.label.includes(vm.filter)) {
+                        vm.labelsDBjsonFiltered.collection.push(labelObj);
+                        return false;
+                    }
+                    return true;
+                });
+            });
+        }
+    }
+
+    function tag(labelObj) {
+        console.log(vm.startOffset);
+        console.log(vm.endOffset);
+    }
+
+    vm.labelsDBjson = {
+        "collection": [
+            {
+                "uri": "text:tanach-1-1-1", "text": "...",
+                "labels": [
+                    {"label": "בראשית א א"},
+                    {"label": "בראשית פרק א פסוק א"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-1-2", "text": "...",
+                "labels": [
+                    {"label": "בראשית א ב"},
+                    {"label": "בראשית פרק א פסוק ב"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-1-3", "text": "...",
+                "labels": [
+                    {"label": "בראשית א ג"},
+                    {"label": "בראשית פרק א פסוק ג"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-1-4", "text": "...",
+                "labels": [
+                    {"label": "בראשית א ד"},
+                    {"label": "בראשית פרק א פסוק ד"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-1-5", "text": "...",
+                "labels": [
+                    {"label": "בראשית א ה"},
+                    {"label": "בראשית פרק א פסוק ה"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-2-1", "text": "...",
+                "labels": [
+                    {"label": "בראשית ב א"},
+                    {"label": "בראשית פרק ב פסוק א"}
+                ]
+            },
+            {
+                "uri": "text:tanach-1-2-2", "text": "...",
+                "labels": [
+                    {"label": "בראשית ב ב"},
+                    {"label": "בראשית פרק ב פסוק ב"}
+                ]
+            },
+            {
+                "uri": "text:tanach-2-1-1", "text": "...",
+                "labels": [
+                    {"label": "שמות א א"},
+                    {"label": "שמות פרק א פסוק א"}
+                ]
+            },
+            {
+                "uri": "text:tanach-2-1-2", "text": "...",
+                "labels": [
+                    {"label": "שמות א ב"},
+                    {"label": "שמות פרק א פסוק ב"}
+                ]
+            },
+            {
+                "uri": "text:tanach-2-1-3", "text": "...",
+                "labels": [
+                    {"label": "שמות א ג"},
+                    {"label": "שמות פרק א פסוק ג"}
+                ]
+            },
+            {
+                "uri": "text:tanach-2-1-4", "text": "...",
+                "labels": [
+                    {"label": "שמות א ד"},
+                    {"label": "שמות פרק א פסוק ד"}
+                ]
+            }
+
+        ]
+    };
+    vm.labelsDB = angular.toJson(vm.labelsDBjson);
+    vm.text = "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\n" +
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla";
+    vm.filter = "";
 }]);
