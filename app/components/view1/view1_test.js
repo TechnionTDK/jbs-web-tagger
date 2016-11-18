fd3fbe6b10967625475ba6415f40d1fe24184546
lchayoun <liel.chayoun@intel.com>
2016-11-18 17:10:22 +0200

    fixed text

--- app/components/view1/view1_test.js
+++ app/components/view1/view1_test.js
@@ -1,16 +1,22 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.view1 module', function () {
 
-  beforeEach(module('myApp.view1'));
+    var View1Ctrl;
 
-  describe('view1 controller', function(){
+    beforeEach(module('myApp.view1'));
+    beforeEach(inject(function($controller,  $rootScope) {
+        // Note the use of the $new() function
+        var $scope = $rootScope.$new();
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+        // The new child scope is passed to the controller's constructor argument
+        View1Ctrl = $controller('View1Ctrl', { $scope: $scope });
     }));
 
-  });
+    describe('view1 controller', function () {
+        it('should ....', inject(function () {
+            //spec body
+            expect(View1Ctrl).toBeDefined();
+        }));
+    });
 });
