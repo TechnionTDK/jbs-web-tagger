1582b7ecf38c479426dc2a3e8fc5f37cfb7d8700
lchayoun <liel.chayoun@intel.com>
2016-11-21 15:39:02 +0200

    removed view2

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -24,19 +24,4 @@ describe('my app', function() {
     });
 
   });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
